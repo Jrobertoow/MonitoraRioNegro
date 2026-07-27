de88606f3bc21cc97eeb62d01f1e62f0a2748eaf
Roberto Garcia <joserobertogarc@gmail.com>
2026-07-26 21:56:59 -0400

    Atualiza run.bat e ajusta execução do projeto

--- run.bat
+++ run.bat
@@ -1,17 +1,22 @@
 @echo off
 chcp 65001 > nul
+
 if not exist bin mkdir bin
 if exist sources.tmp del sources.tmp
+
 for /r src %%f in (*.java) do echo %%f>>sources.tmp
-type sources.tmp
-pause
+
 javac -encoding UTF-8 -d bin @sources.tmp
+
 if errorlevel 1 (
-  del sources.tmp
-  echo Falha na compilacao.
-  pause
-  exit /b 1
+    del sources.tmp
+    echo Falha na compilacao.
+    pause
+    exit /b 1
 )
+
 del sources.tmp
+
 java -cp bin br.com.monitorarionegro.main.Main
+
 pause
